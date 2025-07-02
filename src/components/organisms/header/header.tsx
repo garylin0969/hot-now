31602dbdaf391e056f56a0b039abb0354f70d4bc
Gary Lin <garylin0969@gmail.com>
2025-07-02 15:42:39 +0800

    feat: update RootLayout to include Header component and change language to Chinese

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const header = () => {
-    return <div>header</div>;
+const Header = () => {
+    return (
+        <header className='sticky top-0 left-0 h-14.5 shadow-md backdrop-blur-xl'>
+            <div className='container mx-auto flex h-full items-center justify-between'>
+                <h1 className='text-2xl font-bold'>Hot Now</h1>
+            </div>
+        </header>
+    );
 };
 
-export default header;
+export default Header;
