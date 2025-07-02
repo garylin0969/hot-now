e197d22aee24acc977e636670c82ba3616ff1e14
Gary Lin <garylin0969@gmail.com>
2025-07-02 16:20:09 +0800

    feat: enhance Header component with social media links and icons for improved navigation

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaGoogle, FaLinkedin } from 'react-icons/fa';
+
 import React from 'react';
 
 import { ThemeToggle } from '@/components/atoms/theme-toggle';
@@ -7,7 +9,21 @@ const Header = () => {
         <header className='border-border/40 bg-background/70 sticky top-0 left-0 z-50 h-14.5 border-b shadow-md backdrop-blur-md'>
             <div className='container mx-auto flex h-full items-center justify-between'>
                 <h1 className='text-2xl font-bold'>Hot Now</h1>
-                <ThemeToggle />
+                <nav className='flex items-center gap-2'>
+                    <a href='https://www.google.com' target='_self' rel='noopener noreferrer'>
+                        <FaGoogle className='h-4 w-4' />
+                        <span className='sr-only'>Google</span>
+                    </a>
+                    <a href='https://www.linkedin.com/in/garylin0969' target='_blank' rel='noopener noreferrer'>
+                        <FaLinkedin className='h-4 w-4' />
+                        <span className='sr-only'>LinkedIn</span>
+                    </a>
+                    <a href='https://github.com/garylin0969' target='_blank' rel='noopener noreferrer'>
+                        <FaGithub className='h-4 w-4' />
+                        <span className='sr-only'>GitHub</span>
+                    </a>
+                    <ThemeToggle />
+                </nav>
             </div>
         </header>
     );
