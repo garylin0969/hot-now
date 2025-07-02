d3873f1e52c427fd38c71793331c55611e3d8243
Gary <garylin0969@gmail.com>
2025-07-02 21:59:56 +0800

    refactor: improve variable naming and enhance layout for YouTube and Reddit content display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,27 +5,29 @@ import YoutubeVideoCard from '@/components/molecules/youtube-video-card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
 const Home = async () => {
-    const response = await GetYoutubeHotVideosWithCache();
-    const videos = response?.data?.items || [];
+    // Youtube
+    const youtubeResponse = await GetYoutubeHotVideosWithCache();
+    const youtubeVideos = youtubeResponse?.data?.items || [];
+    // Reddit
     const redditPosts = await GetSimplifiedRedditHotPosts(50);
 
     return (
         <div className="container mx-auto px-4 py-8">
             <Tabs defaultValue="youtube" className="w-full">
-                <TabsList>
+                <TabsList className="mx-auto mb-4">
                     <TabsTrigger value="youtube">Youtube</TabsTrigger>
                     <TabsTrigger value="reddit">Reddit</TabsTrigger>
                 </TabsList>
                 <TabsContent value="youtube">
-                    <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
-                        {videos.map((video) => (
+                    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5">
+                        {youtubeVideos?.map((video) => (
                             <YoutubeVideoCard key={video.id} video={video} />
                         ))}
                     </div>
                 </TabsContent>
                 <TabsContent value="reddit">
                     <div className="mx-auto flex max-w-4xl flex-col gap-4">
-                        {redditPosts.map((post) => (
+                        {redditPosts?.map((post) => (
                             <RedditPostCard key={post.id} post={post} />
                         ))}
                     </div>
