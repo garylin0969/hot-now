ba5e2dd3deb59c3b2a273360d50325f233bdf07d
Gary <garylin0969@gmail.com>
2025-07-03 21:53:16 +0800

    style: update header title styling to use primary color and extrabold font for improved visibility

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <header className="border-border/40 bg-background/70 sticky top-0 left-0 z-50 h-14.5 border-b shadow-md backdrop-blur-md">
             <div className="container mx-auto flex h-full items-center justify-between">
-                <h1 className="text-2xl font-bold">Hot Now</h1>
+                <h1 className="text-primary text-2xl font-extrabold">Hot Now</h1>
                 <nav className="flex items-center gap-2">
                     <a href="https://www.google.com" target="_self" rel="noopener noreferrer">
                         <FaGoogle className="h-4 w-4" />
