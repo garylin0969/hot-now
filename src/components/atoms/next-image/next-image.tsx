36a4008ac8a50c690f155c236eb4741f6888d75f
Gary Lin <garylin0969@gmail.com>
2025-07-17 08:07:02 +0800

    refactor: update NextImage component to accept unoptimized prop for better image handling

--- src/components/atoms/next-image/next-image.tsx
+++ src/components/atoms/next-image/next-image.tsx
@@ -2,10 +2,10 @@ import Image, { ImageProps } from 'next/image';
 
 const DEFAULT_IMAGE_NOT_FOUND = '/image-not-found.png';
 
-const NextImage = ({ src, ...props }: ImageProps) => {
+const NextImage = ({ src, unoptimized = true, ...props }: ImageProps) => {
     const imgSrc = src || DEFAULT_IMAGE_NOT_FOUND;
 
-    return <Image src={imgSrc} {...props} unoptimized />;
+    return <Image src={imgSrc} unoptimized={unoptimized} {...props} />;
 };
 
 export default NextImage;
